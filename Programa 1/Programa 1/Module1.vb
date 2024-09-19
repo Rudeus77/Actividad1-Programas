7a045d6c07b3c072b2e49d9c851e54430421147f
Rudy Hernandez <2523166@clases.edu.sv>
2024-09-19 15:56:56 -0600

    Ejercicio 1 Programa pra verificar si una persona es mayor de edad.

---
+++ Programa 1/Programa 1/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim edadIngresada As Integer = 0
+        Dim mayorEdad As Integer = 18
+
+        Console.Write("Ingrese su edad por favor: ")
+        edadIngresada = Console.ReadLine()
+
+        If edadIngresada >= mayorEdad Then
+            Console.WriteLine("Eres mayor de edad.")
+
+        Else
+            Console.WriteLine("Eres menor de edad.")
+
+        End If
+
+
+
+
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
