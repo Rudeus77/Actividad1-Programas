2e33326277cbe1bafee6c73993f194cdb69a97a8
Rudy Hernandez <2523166@clases.edu.sv>
2024-09-21 10:37:01 -0600

    Creacion de programa que indica si un numero es positivo, negativo o cero.

---
+++ Programa 2/Programa 2/Module1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numeroIngresado As Double
+
+
+        Console.Write("Ingrese un número: ")
+        Dim input As String = Console.ReadLine()
+
+
+        If Double.TryParse(input, numeroIngresado) Then
+            If numeroIngresado > 0 Then
+                Console.WriteLine("Tu numero es positivo.")
+
+            ElseIf numeroIngresado < 0 Then
+                Console.WriteLine("Tu numero es negativo.")
+
+            Else
+                Console.WriteLine("Tu numero es cero")
+
+            End If
+
+        End If
+
+
+
+
+
+            Console.ReadKey()
+
+    End Sub
+
+End Module
